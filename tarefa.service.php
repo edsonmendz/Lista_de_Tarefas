7e4b1e04df5d85d4567c78f30c298a82a1f49ea8
edsonmendz <japaact@gmail.com>
2023-06-12 18:41:53 -0300

    Método Post

---
+++ tarefa.service.php
@@ -0,0 +1,19 @@
+<?php
+    class TarefaService {
+        public function inserir() { // CREATE
+
+        }
+
+        public function recuperar() { // READ
+
+        }
+
+        public function atualizar() { // UPDATE
+
+        }
+
+        public function remover() { // DELETE
+
+        }
+    }
+?>
---
+++ tarefa_controller.php
@@ -0,0 +1,9 @@
+<?php
+    //require_once '../../app_lista_tarefas/tarefa_controller.php'
+?>
+
+<?php
+    echo '<pre>';
+    print_r($_POST);
+    echo '</pre>';
+?>
