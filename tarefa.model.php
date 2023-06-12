827c4cb8d4905d74dda53bcf859f61858d57b77f
edsonmendz <japaact@gmail.com>
2023-06-12 20:34:45 -0300

    Confirmação de inserção

--- tarefa.model.php
+++ tarefa.model.php
@@ -5,14 +5,14 @@
         private $id_status;
         private $tarefa;
         private $data_cadastro;
-    }
 
-    public function __get($atributo) {
-        return $this->$atributo;
-    }
-
-    public function __get($atributo, $valor) {
-        $this->$atributo = $valor;
-    }
+        public function __get($atributo) {
+            return $this->$atributo;
+        }
+    
+        public function __set($atributo, $valor) {
+            $this->$atributo = $valor;
+        }
+    }    
 
 ?>
--- tarefa_controller.php
+++ tarefa_controller.php
@@ -3,7 +3,18 @@
 ?>
 
 <?php
-    echo '<pre>';
-    print_r($_POST);
-    echo '</pre>';
+    require "tarefa.model.php";
+    require "tarefa.service.php";
+    require "conexao.php";
+
+    
+    $tarefa = new Tarefa();
+    $tarefa->__set('tarefa', $_POST['tarefa']);
+
+    $conexao = new Conexao();
+
+    $tarefaService = new TarefaService($conexao, $tarefa);
+    $tarefaService->inserir();
+
+    header('Location: nova_tarefa.php?inclusao=1');
 ?>
